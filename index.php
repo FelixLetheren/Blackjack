6c2694a91dd753e5e8b66a5e5cec4ab0d803ac79
Felix Letheren <felix.letheren@gmail.com>
2019-07-24 14:36:18 +0100

    Added blank game board

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>BlackJack(ish)</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body>
+<div class="container">
+    <h1>Blackjack for Nerds</h1>
+    <div class="player1-container">
+    </div>
+    <div class="player2-container">
+
+    </div>
+    <div class="results-container">
+
+    </div>
+</div>
 
 </body>
 </html>
